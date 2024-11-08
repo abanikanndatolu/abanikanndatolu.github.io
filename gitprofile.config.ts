24e9fdcc2ea02463686d0185a145056c41235f74
Tolulope Abanikannda <abanikanndatolulope@gmail.com>
2024-11-08 22:37:33 +0100

    site files

---
+++ gitprofile.config.ts
@@ -0,0 +1,161 @@
+// gitprofile.config.ts
+
+const CONFIG = {
+  github: {
+    username: 'abanikanndatolu', // Your GitHub org/user name. (This is the only required config)
+  },
+ 
+  base: '/',
+  projects: {
+    github: {
+      display: true, // Display GitHub projects?
+      header: 'My Projects',
+      mode: 'automatic', // Mode can be: 'automatic' or 'manual'
+      automatic: {
+        sortBy: 'stars', // Sort projects by 'stars' or 'updated'
+        limit: 8, // How many projects to display.
+        exclude: {
+          forks: false, // Forked projects will not be displayed if set to true.
+          projects: [], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        },
+      },
+      manual: {
+        // Properties for manually specifying projects
+        projects: [], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+      },
+    },
+    external: {
+      header: 'My Projects',
+      // To hide the `External Projects` section, keep it empty.
+      projects: [],
+    },
+  },
+  seo: {
+    title: 'Portfolio of AbanikanndaTolu',
+    description: '',
+    imageURL: '',
+  },
+  social: {
+    linkedin: 'abanikanndatolu',
+    twitter: 'abanikanndatolu',
+    mastodon: '',
+    researchGate: '',
+    facebook: 'abanikanndatolu',
+    instagram: 'abanikanndatolu',
+    reddit: '',
+    threads: '',
+    youtube: '', // example: 'pewdiepie'
+    udemy: '',
+    dribbble: '',
+    behance: 'abanikanndatolu',
+    medium: 'abanikanndatolu',
+    dev: '',
+    stackoverflow: '', // example: '1/jeff-atwood'
+    skype: '',
+    telegram: 'abanikanndatolu',
+    website: '',
+    phone: '+2348148226089',
+    email: '',
+  },
+  resume: {
+    fileUrl:
+      '', // Empty fileUrl will hide the `Download Resume` button.
+  },
+  skills: [
+    'PHP',
+    'MySql',
+    'JavaScript',
+    'Python',
+    'Node.js',
+	'Flutter',
+	'Linux',
+	'Arduino',
+  ],
+  experiences: [],
+  certifications: [],
+  educations: [],
+  publications: [],
+  // Display articles from your medium or dev account. (Optional)
+  blog: {
+    source: 'dev', // medium | dev
+    username: '', // to hide blog section, keep it empty
+    limit: 2, // How many articles to display. Max is 10.
+  },
+  googleAnalytics: {
+    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+  },
+  // Track visitor interaction and behavior. https://www.hotjar.com
+  hotjar: {
+    id: '',
+    snippetVersion: 6,
+  },
+  themeConfig: {
+    defaultTheme: 'synthwave',
+
+    // Hides the switch in the navbar
+    // Useful if you want to support a single color mode
+    disableSwitch: false,
+
+    // Should use the prefers-color-scheme media-query,
+    // using user system preferences, instead of the hardcoded defaultTheme
+    respectPrefersColorScheme: false,
+
+    // Display the ring in Profile picture
+    displayAvatarRing: true,
+
+    // Available themes. To remove any theme, exclude from here.
+    themes: [
+      'light',
+      'dark',
+      'cupcake',
+      'bumblebee',
+      'emerald',
+      'corporate',
+      'synthwave',
+      'retro',
+      'cyberpunk',
+      'valentine',
+      'halloween',
+      'garden',
+      'forest',
+      'aqua',
+      'lofi',
+      'pastel',
+      'fantasy',
+      'wireframe',
+      'black',
+      'luxury',
+      'dracula',
+      'cmyk',
+      'autumn',
+      'business',
+      'acid',
+      'lemonade',
+      'night',
+      'coffee',
+      'winter',
+      'dim',
+      'nord',
+      'sunset',
+      'procyon',
+    ],
+
+    // Custom theme, applied to `procyon` theme
+    customTheme: {
+      primary: '#fc055b',
+      secondary: '#219aaf',
+      accent: '#e8d03a',
+      neutral: '#2A2730',
+      'base-100': '#E3E3ED',
+      '--rounded-box': '3rem',
+      '--rounded-btn': '3rem',
+    },
+  },
+
+  // Optional Footer. Supports plain text or HTML.
+  footer: `This Is The End Of My Profile`,
+
+  enablePWA: true,
+};
+
+export default CONFIG;
